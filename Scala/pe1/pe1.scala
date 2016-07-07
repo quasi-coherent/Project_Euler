00905192091adf099d822993f2491f613b519d21
quasi-coherent <d.michael.donohue@gmail.com>
2016-07-07 14:45:52 -0400

    Minor edit

--- Scala/pe1/pe1.scala
+++ Scala/pe1/pe1.scala
@@ -8,7 +8,7 @@ Find the sum of all the multiples of 3 or 5 below 1000. */
 
 object ProjectEulerOne {
 	def filterSum(xs: Array[Int], f: Int => Boolean): Int = 
-	  xs.filter(f(_)).reduce(_ + _)
+	  xs.filter(f(_)).sum
 
 	def main(args: Array[String]): Unit = {
 		println(filterSum((1 until 1000).toArray, x => x%3 == 0 || x%5 == 0))
